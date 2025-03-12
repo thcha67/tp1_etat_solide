6a7d15ce1d6986975e3ebcdb3320c1c9fc20fe5b
Laurent <laemo10@ulaval.ca>
2025-03-12 16:37:48 -0400

    ajout frequency step

--- tp2/p2.py
+++ tp2/p2.py
@@ -14,7 +14,7 @@ images = [plt.imread(f) for f in tif_files]
 
 # fft of each image
 
-for im in images[-1:]:
+for im in images[:]:
 
     plt.imshow(im)
     plt.show()
@@ -22,28 +22,35 @@ for im in images[-1:]:
     fft = np.fft.fft2(im)
     fft = np.fft.fftshift(fft)
 
-    # Log scale for better visualization
+    # Cela converti un pixel dans la transfo de fourrier en mm-1. La distance entre 2 point est désormais en mm-1. 
+    spatial_res = 42.468 * 10**(-12)
+    freq_res = 1/(1024 * spatial_res)    
+    freq = np.fft.fftfreq(1024, d=spatial_res)
+    freq = np.fft.fftshift(freq)
+
     fft_magnitude = np.abs(fft)
     sigma = 4
     filtered_fft = gaussian_filter(fft_magnitude, sigma=sigma)
     fft_log = np.log(filtered_fft + 1)
 
-    # Display results
-    plt.figure(figsize=(10,5))
-    plt.subplot(1,2,1)
-    plt.title("Original Diffraction Pattern")
-    plt.imshow(np.log(1 + np.abs(fft)), cmap='gray')
+    
+    COMPARISON_FFT = False
+    if COMPARISON_FFT:
+        # Display results
+        plt.figure(figsize=(10,5))
+        plt.subplot(1,2,1)
+        plt.title("Original Diffraction Pattern")
+        plt.imshow(np.log(1 + np.abs(fft)), cmap='gray')
 
-    plt.subplot(1,2,2)
-    plt.title("Filtered Diffraction Pattern")
-    plt.imshow(np.log(1 + np.abs(filtered_fft)), cmap='gray')
-    plt.show()
+        plt.subplot(1,2,2)
+        plt.title("Filtered Diffraction Pattern")
+        plt.imshow(np.log(1 + np.abs(filtered_fft)), cmap='gray')
+        plt.show()
 
     # Find local maxima in the FFT image (spots)
     # Use a threshold to avoid detecting noise
-    threshold = np.percentile(fft_log, 99.2)  # Can adjust this as needed
-    spots = peak_local_max(fft_log, min_distance=20, threshold_abs=threshold)
-    print(spots)
+    threshold = np.percentile(filtered_fft, 99.2)  # Can adjust this as needed
+    spots = peak_local_max(filtered_fft, min_distance=20, threshold_abs=threshold)
 
     # Display the identified spots
     plt.figure(figsize=(6, 6))
@@ -51,7 +58,8 @@ for im in images[-1:]:
     plt.scatter(spots[:, 1], spots[:, 0], color='red', s=20)  # Plot spots
     # Annotate each point with its coordinates
     for i, (y_val, x_val) in enumerate(spots):
-        #plt.text(x_val, y_val - 15, f"({x_val},{y_val})", fontsize=12 , color='red', ha = "center")
+        print(f"Noeud[{i}] = ({freq[x_val]:.2e} mm-1,{freq[y_val]:.2e} mm-1)")
+        plt.text(x_val, y_val - 15, f"({freq[x_val]:.2e},{freq[y_val]:.2e})", fontsize=12 , color='red', ha = "center")
         pass
     plt.title("Reciprocal Lattice")
     plt.xlabel("Pixels [-]", fontsize = 14)
@@ -60,9 +68,6 @@ for im in images[-1:]:
 
     ### TODO : Link Pattern to what is requested in the TP
 
-    #1 Rectangulaire
-    #2
-    #3 Oblique
 
     """
 
@@ -92,5 +97,10 @@ for im in images[-1:]:
 
     plt.imshow(np.abs(ifft), cmap='gray')
     plt.show()
+
+
+    [228.47, 228.47, 228.39, 228.29, 228.47, 228.47]
+
+    [136.71, 137.72, 135.62, 136.71, 136.71, 135.62]
 
     """
